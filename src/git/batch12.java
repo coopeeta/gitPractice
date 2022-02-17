037d5673517af1e7c68bf74e7182c3889a107bf9
coopeeta <coopeeta@gmail.com>
2022-02-17 13:28:53 -0500

    i have updated some code

--- src/git/batch12.java
+++ src/git/batch12.java
@@ -5,6 +5,7 @@ public class batch12 {
 	public static void main(String[] args) {
 
 		System.out.println("Places to visit: Barcelona, Belize, Bahamas");
+		System.out.println("US places to visit: Savannah, New Orleans, West Coast");
 	}
 
 }
